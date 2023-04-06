b476b3f1bcc80d1cb2cdfcbfd319eca500897377
SurajPahadi <2002surajpandey@gmail.com>
2023-04-06 10:34:35 +0530

    view button

--- src/components/Textform.jsx
+++ src/components/Textform.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 
 export default function Textform() {
     //               text is  value and useState is method    (and)     useState is use for updating text value
-    const [text, setText] = useState("Enter your text");
+    const [text, setText] = useState("");
 
     // function ....
     const handleOnChange = (event) => {
@@ -20,11 +20,15 @@ export default function Textform() {
         let newText = text.toLowerCase();
         setText("");
     }
+    const btn4 = () => {
+        let newText = text.toLowerCase();
+        setText("");
+    }
 
     return (
         <div>
             <div className="mb-3">
-            {/* css use in jsx     "style={{margin:'50px', padding:'10px;'}}"     */}
+                {/* css use in jsx     "style={{margin:'50px', padding:'10px;'}}"     */}
                 <h1>Enter text here</h1>
                 <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"
                     value={text} onChange={handleOnChange}></textarea>
@@ -32,6 +36,13 @@ export default function Textform() {
                     <button className='btn btn-primary m-1' onClick={btn1}>UPPER CASE</button>
                     <button className='btn btn-primary m-1' onClick={btn2}>LOWER CASE</button>
                     <button className='btn btn-primary m-1' onClick={btn3}>CLEAR</button>
+                    <button className='btn btn-primary m-1' onClick={btn4}>VIEW</button>
+                </div>
+                <div>
+                    <h2>Your text Summary</h2>
+                </div>
+                <div>
+                    <p>total character {text.split(" ").length } and total words {text.length}</p>
                 </div>
             </div>
         </div>
