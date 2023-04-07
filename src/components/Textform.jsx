ecb35c181f968583190da040dc7384f75f5bc828
SurajPahadi <2002surajpandey@gmail.com>
2023-04-07 09:28:58 +0530

    add a button that capitalize first word of sentence

--- src/components/Textform.jsx
+++ src/components/Textform.jsx
@@ -17,34 +17,43 @@ export default function Textform() {
         setText(newText);
     }
     const btn3 = () => {
-        let newText = text.toLowerCase();
         setText("");
     }
     const btn4 = () => {
-        let newText = text.toLowerCase();
-        setText("");
+        let properForm = text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
+        setText(properForm);
     }
-
-    return (
-        <div>
-            <div className="mb-3">
-                {/* css use in jsx     "style={{margin:'50px', padding:'10px;'}}"     */}
-                <h1>Enter text here</h1>
-                <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"
-                    value={text} onChange={handleOnChange}></textarea>
-                <div className='p-2'>
-                    <button className='btn btn-primary m-1' onClick={btn1}>UPPER CASE</button>
-                    <button className='btn btn-primary m-1' onClick={btn2}>LOWER CASE</button>
-                    <button className='btn btn-primary m-1' onClick={btn3}>CLEAR</button>
-                    <button className='btn btn-primary m-1' onClick={btn4}>VIEW</button>
-                </div>
-                <div>
-                    <h2>Your text Summary</h2>
-                </div>
-                <div>
-                    <p>total character {text.split(" ").length } and total words {text.length}</p>
-                </div>
+    
+    // const btn4 = () => {
+    //     let properForm = text.split(" ");
+    //     let temp = "";
+    //     properForm.forEach((element)=>{
+    //         let element = element.charAt(0).toUpperCase() + element.substr(1,properForm.length).toLowerCase();
+    //         temp   = temp +" "+ element;
+    //         setText(temp);
+    //     })
+       
+    }
+return (
+    <div>
+        <div className="mb-3 container">
+            {/* css use in jsx     "style={{margin:'50px', padding:'10px;'}}"     */}
+            <h1>Enter text here</h1>
+            <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"
+                value={text} onChange={handleOnChange}></textarea>
+            <div className='p-2'>
+                <button className='btn btn-primary m-1' onClick={btn1}>UPPER CASE</button>
+                <button className='btn btn-primary m-1' onClick={btn2}>LOWER CASE</button>
+                <button className='btn btn-primary m-1' onClick={btn3}>CLEAR</button>
+                <button className='btn btn-primary m-1' onClick={btn4}>PROPER FORM</button>
+            </div>
+            <div>
+                <h2>Your text Summary</h2>
+            </div>
+            <div>
+                <p>total character {text.split(" ").length} and total words {text.length}</p>
             </div>
         </div>
-    )
+    </div>
+)
 }
